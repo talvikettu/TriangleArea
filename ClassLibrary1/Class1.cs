d3ae794f92bd86fea58e740077d7cbe7a540270b
dinoden <dinozer1234@gmail.com>
2024-10-05 15:25:10 +0300

    git commit

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -1,21 +1,33 @@
 ﻿using System;
 
-namespace TriangleCalc
+namespace Calc
 {
     public class Triangle
     {
         public static double Calculate(double A, double B, double C)
         {
-            if (A <= 0 || B <= 0 || C <= 0) // проверка на корректность введёных данных
+            if (A <= 0 || B <= 0 || C <= 0) 
             {
                 throw new ArgumentException("Стороны треугольника должны быть положительными.");
             }
-            if (A + B <= C || A + C <= B || B + C <= A) // проверка на то, что это треугольник
+            if (A + B <= C || A + C <= B || B + C <= A) 
             {
                 throw new ArgumentException("Заданные стороны не могут образовать треугольник.");
             }
-            double semiperimeter = (A + B + C) / 2; // полупериметр для формулы Герона
+            double semiperimeter = (A + B + C) / 2; 
             double area = Math.Sqrt(semiperimeter * (semiperimeter - A) * (semiperimeter - B) * (semiperimeter - C));
+            return area;
+        }
+    }
+    public class Circle
+    {
+        public static double Calculate(double radius)
+        {
+            if(radius<= 0)
+            {
+                throw new ArgumentException("Радиус должен быть больше нуля");
+            }
+            double area = double.Pi * radius * radius;
             return area;
         }
     }
